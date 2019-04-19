0dfd078bf00f997351714b915ce6440ad104d3d0
M.Kozak <kozak_m@outlook.com>
2019-04-19 14:07:22 +0200

    routingRequests

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers)
-    //process.exit()
+    const url = req.url;
+
+    if(url==='/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My first...</title></head>');
+        res.write('<body><form action="/message" methode="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My first...</title></head>')
-    res.write('<body><h1>Hello from my node.js server.</h1></body>')
-    res.write('</html>')
+    res.write('<html>');
+    res.write('<head><title>My first...</title></head>');
+    res.write('<body><h1>Hello from my node.js server.</h1></body>');
+    res.write('</html>');
     res.end()
+    
 })
 
 server.listen(3000)
