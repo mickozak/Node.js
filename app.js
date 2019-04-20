b0b5a01e5249353a21246e0c7a32e0c89d73f043
M.Kozak <kozak_m@outlook.com>
2019-04-20 20:37:51 +0200

    redirectingReuest

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     const url = req.url;
-
+    const method = req.methode
     if(url==='/'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My first...</title></head>');
         res.write('<body><form action="/message" methode="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        return res.end()
+        return res.end();
+    }
+    if(url==='/message'&& method === 'POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode('302');
+        res.setHeader('Location','/');
+        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first...</title></head>');
     res.write('<body><h1>Hello from my node.js server.</h1></body>');
     res.write('</html>');
-    res.end()
-    
+    res.end();
 })
 
 server.listen(3000)
