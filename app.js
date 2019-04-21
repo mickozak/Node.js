ef1ce673ce5f6530c4d518b530770d08a369d5d9
M.Kozak <kozak_m@outlook.com>
2019-04-21 14:06:34 +0200

    parsingRequestBodies

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
+//import start
+
 const http = require('http');
 const fs = require('fs');
 
+//import end
+
 const server = http.createServer((req,res)=>{
     const url = req.url;
-    const method = req.methode
+    const method = req.method;
     if(url==='/'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>My first...</title></head>');
-        res.write('<body><form action="/message" methode="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     if(url==='/message'&& method === 'POST'){
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode('302');
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk)
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        })
+        res.statusCode=302;
         res.setHeader('Location','/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head><title>My first...</title></head>');
+    res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my node.js server.</h1></body>');
     res.write('</html>');
     res.end();
