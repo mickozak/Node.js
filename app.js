3bea05521c84913a35b2669d8acab832cddef501
M.Kozak <kozak_m@outlook.com>
2019-04-25 21:57:18 +0200

    middlewareNextSend

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+    next();
 });
 
 app.use((req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.send('<h1>Hello!<h1>')
 });
 
 const server = http.createServer(app);
