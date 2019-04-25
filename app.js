83ad2fbf7e15cdd399cb8f38ff494feaf266d0cd
M.Kozak <kozak_m@outlook.com>
2019-04-25 22:38:32 +0200

    parsing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
 //app.use((req, res, next) => {
@@ -9,15 +11,22 @@ const app = express();
     //next();
 //});
 
-app.use('/',(req,res,next)=>{
-    console.log('This always runs!');
-    next();
-})
+//app.use('/',(req,res,next)=>{
+  //  console.log('This always runs!');
+    //next();
+//})
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/add-product',(req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page<h1>')
+    res.send('<form><input action="/product" method="POST" type="text" name="title"><button type="submit">SEND</button><form>')
 });
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
 
 app.use('/',(req, res, next) => {
     console.log('In another middleware!');
@@ -28,4 +37,4 @@ app.use('/',(req, res, next) => {
 
 //server.listen(8000);
 
-app.listen(8000);
+app.listen(4000);
