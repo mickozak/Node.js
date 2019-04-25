ce4444d5b01cf1dfb3b49c21f46094a621e1ab82
M.Kozak <kozak_m@outlook.com>
2019-04-25 22:17:29 +0200

    handlingDifferentRouters

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+//app.use((req, res, next) => {
+  //  console.log('In the middleware!');
+    //next();
+//});
+
+app.use('/',(req,res,next)=>{
+    console.log('This always runs!');
     next();
+})
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page<h1>')
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello!<h1>')
+    res.send('<h1>Hello from Express!<h1>')
 });
 
 //const server = http.createServer(app);
