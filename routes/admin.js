d117c8239aceeb6a63c4b3be379e0b4955dd052b
M.Kozak <kozak_m@outlook.com>
2019-05-13 21:02:47 +0200

    html&serving2

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,15 @@
-const path = require('path')
+const path = require('path');
 
 const express = require('express');
 
 const router = express.Router();
 
-
-//GET
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'admin'));
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-//POST
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
